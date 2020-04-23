c1dad023ff9032ea5e2ccb0c01d3f9ac5e4e168c
sem194 <sem194@txstate.edu>
2020-04-23 10:47:42 -0500

    Schemas created for Login and Register forms for ORM/ODM. Removed forms.py because did not meet requirements for MVC. Controllers still need to be implemented if not satisfied by routes (will investigate).

--- app/models/mongo_setup.py
+++
@@ -1,6 +0,0 @@
-import mongoengine
-
-
-def global_init():
-    # database name: slither
-    mongoengine.register_connection(alias='core', name='slither')
--- app/models/registerUserSchema.py
+++ app/models/registerUserSchema.py
@@ -17,9 +17,3 @@ class LoginForm(FlaskForm):
     username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
     password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
     remember = BooleanField('remember me')
-
-
-class RegisterForm(FlaskForm):
-    email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
-    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
-    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
